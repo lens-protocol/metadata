c61610fb252fc1631fbe67a245ebe7fa741bdbb4
Cesare Naldi <naldi.cesare@gmail.com>
2023-08-16 00:05:43 +0200

    Adds tsup

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-default-export */
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => ({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  splitting: false,
+  sourcemap: true,
+  treeshake: true,
+  clean: true,
+  bundle: true,
+  minify: !options.watch,
+  dts: true,
+  platform: 'neutral',
+  format: ['cjs', 'esm'],
+}));
