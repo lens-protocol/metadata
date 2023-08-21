095a6b252378253b1c4199368b6c34326259c35f
Cesare Naldi <naldi.cesare@gmail.com>
2023-08-21 12:57:27 +0200

    Adds basic test coverage

--- src/publication/__tests__/PublicationMetadataSchema.spec.ts
+++ src/publication/__tests__/PublicationMetadataSchema.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it } from '@jest/globals';
 
 import { expectSchema } from '../../__helpers__/assertions.js';
-import { PublicationMainFocus, PublicationMetadataSchema, PublicationSchemaId } from '../index.js';
+import { PublicationMetadataSchema, PublicationSchemaId } from '../index.js';
 
 describe(`Given the PublicationMetadataSchema`, () => {
   describe(`when parsing an empty object`, () => {
@@ -18,15 +18,241 @@ describe(`Given the PublicationMetadataSchema`, () => {
       expectSchema(() =>
         PublicationMetadataSchema.safeParse({
           $schema: PublicationSchemaId.ARTICLE_LATEST,
-          lens: {
-            mainContentFocus: PublicationMainFocus.ARTICLE,
-          },
+          lens: {},
         }),
       ).toMatchInlineSnapshot(`
         "fix the following issues
         · "lens.id": Required
         · "lens.content": Required
-        · "lens.locale": Required"
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "ARTICLE""
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.AUDIO_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.AUDIO_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "AUDIO"
+        · "lens.audio": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.CHECKING_IN_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.CHECKING_IN_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "CHECKING_IN"
+        · "lens.location": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.EMBED_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.EMBED_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "EMBED"
+        · "lens.embed": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.IMAGE_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.IMAGE_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "IMAGE"
+        · "lens.image": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.LINK_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.LINK_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "LINK"
+        · "lens.sharingLink": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.LIVESTREAM_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.LIVESTREAM_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "LIVESTREAM"
+        · "lens.startsAt": Required
+        · "lens.playbackUrl": Required
+        · "lens.liveUrl": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.MINT_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.MINT_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "MINT"
+        · "lens.mintLink": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.SPACE_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.SPACE_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "SPACE"
+        · "lens.link": Required
+        · "lens.startsAt": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.STORY_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.STORY_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "STORY""
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.TEXT_ONLY_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.TEXT_ONLY_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.content": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "TEXT_ONLY""
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.THREE_D_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.THREE_D_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "3D"
+        · "lens.threeDAssets": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.TRANSACTION_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.TRANSACTION_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus": Invalid literal value, expected "TRANSACTION"
+        · "lens.txHash": Required
+        · "lens.chainId": Required"
+      `);
+    });
+  });
+
+  describe(`when parsing an invalid ${PublicationSchemaId.VIDEO_LATEST}`, () => {
+    it(`then should flag the missing fields`, () => {
+      expectSchema(() =>
+        PublicationMetadataSchema.safeParse({
+          $schema: PublicationSchemaId.VIDEO_LATEST,
+          lens: {},
+        }),
+      ).toMatchInlineSnapshot(`
+        "fix the following issues
+        · "lens.id": Required
+        · "lens.locale": Required
+        · "lens.mainContentFocus" expected to match one of the following groups:
+        		· "lens.mainContentFocus": Invalid literal value, expected "SHORT_VIDEO"
+        	OR:
+        		· "lens.mainContentFocus": Invalid literal value, expected "VIDEO"
+        · "lens.video": Required"
       `);
     });
   });
