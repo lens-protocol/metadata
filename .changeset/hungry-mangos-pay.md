d0cd5400c3392d13733795335aa93c2c776c3af4
Cesare Naldi <naldi.cesare@gmail.com>
2023-09-12 10:53:56 +0200

    Fixes NftOwnershipCondition so to include restriction around ERC-1155

---
+++ .changeset/hungry-mangos-pay.md
@@ -0,0 +1,5 @@
+---
+'@lens-protocol/metadata': patch
+---
+
+**Fixed** `NftOwnershipCondition` to include ERC-1155 limitations around token IDs
---
+++ src/__helpers__/mocks.ts
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker';
+
+import {
+  EvmAddress,
+  NetworkAddress,
+  ProfileId,
+  toChainId,
+  toEvmAddress,
+  toProfileId,
+} from '../primitives';
+
+export function mockProfileId(): ProfileId {
+  return toProfileId(faker.string.hexadecimal({ length: 2 }));
+}
+
+export function mockEvmAddress(): EvmAddress {
+  return toEvmAddress(faker.string.hexadecimal({ length: 32 }));
+}
+
+export function mockNetworkAddress(overrides?: Partial<NetworkAddress>): NetworkAddress {
+  return {
+    address: mockEvmAddress(),
+    chainId: toChainId(1),
+    ...overrides,
+  };
+}
