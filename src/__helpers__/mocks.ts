7ad9d01f974ead17f5cca72c0717b8b69be68acb
Cesare Naldi <naldi.cesare@gmail.com>
2025-01-13 18:34:58 +0100

    chore: removes unused primitives

--- src/__helpers__/mocks.ts
+++ src/__helpers__/mocks.ts
@@ -1,26 +1,7 @@
 import { faker } from '@faker-js/faker';
 
-import {
-  type EvmAddress,
-  type LegacyProfileId,
-  type NetworkAddress,
-  toChainId,
-  toEvmAddress,
-  toProfileId,
-} from '../primitives';
-
-export function mockProfileId(): LegacyProfileId {
-  return toProfileId(faker.string.hexadecimal({ length: 2 }));
-}
+import { type EvmAddress, toEvmAddress } from '../primitives';
 
 export function mockEvmAddress(): EvmAddress {
   return toEvmAddress(faker.string.hexadecimal({ length: 40 }));
-}
-
-export function mockNetworkAddress(overrides?: Partial<NetworkAddress>): NetworkAddress {
-  return {
-    address: mockEvmAddress(),
-    chainId: toChainId(1),
-    ...overrides,
-  };
 }
